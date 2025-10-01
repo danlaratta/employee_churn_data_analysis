1c50a5b28989ef7ab542ccd98653560514520339
danlaratta <dan.laratta.jr@gmail.com>
2025-10-01 15:38:34 -0400

    Analysis update
    completed pandas for questions 2 and 3 of my analysis. Question 1 and 4 remain plus visuals

--- etl/__init__.py
+++ etl/__init__.py
@@ -1,5 +1,6 @@
 from .clean_data import clean_employee_data, clean_store_data
 from .load import export_dfs, load_to_postgres
 from .extract import get_raw_data
+from .db_utils import get_engine, write_df
 
-__all__ = ['clean_employee_data', 'clean_store_data', 'export_dfs', 'load_to_postgres', 'get_raw_data']
+__all__ = ['clean_employee_data', 'clean_store_data', 'export_dfs', 'load_to_postgres', 'get_raw_data', 'get_engine', 'write_df']
